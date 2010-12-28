79cc8dbfb553ba2b19ba4041c70d99e06cd984ba
Jim Safley <jimsafley@gmail.com>
2010-12-28 06:36:10 +0000

    Cleans up item relation form and secondary rendering code.

--- plugin.php
+++ plugin.php
@@ -146,52 +146,13 @@ class ItemRelationsPlugin
         
         $subjects = $db->getTable('ItemRelationsItemRelation')->findBySubjectItemId($item->id);
         $objects = $db->getTable('ItemRelationsItemRelation')->findByObjectItemId($item->id);
-?>
-<div class="info-panel">
-    <h2>Item Relations</h2>
-    <div>
-        <?php if ($subjects || $objects): ?>
-        <ul>
-            <?php foreach ($subjects as $subject): ?>
-            <?php
-            $title = item('Dublin Core', 'Title', array(), get_item_by_id($subject->object_item_id));
-            if (!$title) {
-                $title = $subject->object_item_id;
-            }
-            ?>
-            <li>This Item <strong><?php echo $subject->name; ?></strong> <a href="<?php echo uri('items/show/' . $subject->object_item_id); ?>" target="_blank"><?php echo $title; ?></a></li>
-            <?php endforeach; ?>
-            <?php foreach ($objects as $object): ?>
-            <?php
-            $title = item('Dublin Core', 'Title', array(), get_item_by_id($object->subject_item_id));
-            if (!$title) {
-                $title = $object->subject_item_id;
-            }
-            ?>
-            <li><a href="<?php echo uri('items/show/' . $object->subject_item_id); ?>" target="_blank"><?php echo $title; ?></a> <strong><?php echo $object->name; ?></strong> This Item</li>
-            <?php endforeach; ?>
-        </ul>
-        <?php else: ?>
-        <p>This item has no relations.</p>
-        <?php endif; ?>
-    </div>
-</div>
-<?php
+        include 'item_relations_secondary.php';
     }
     
     public static function adminItemsFormTabs($tabs, $item)
     {
-        $tabContent = '';
-        $tabContent .= self::itemRelationsFormScripts();
-        $tabContent .= self::itemRelationsFormContent($item);
-        $tabs['Item Relations'] = $tabContent;
+        $tabs['Item Relations'] = self::itemRelationsFormContent($item);
         return $tabs;
-    }
-    
-    // Put "Add Relation" JavaScript here.
-    public static function itemRelationsFormScripts()
-    {
-        return '';
     }
     
     public static function itemRelationsFormContent($item)
@@ -203,66 +164,7 @@ class ItemRelationsPlugin
         $objects = $db->getTable('ItemRelationsItemRelation')->findByObjectItemId($item->id);
         
         ob_start();
-?>
-<div>
-    <div class="item-relations-entry">This Item 
-    <select name="item_relations_relation_id[]">
-        <option value="">Select below...</option>
-        <?php foreach ($relations as $relation): ?>
-        <option value="<?php echo $relation->id; ?>"><?php echo $relation->name; ?></option>
-        <?php endforeach; ?>
-    </select> 
-    Item ID <input type="text" name="item_relations_item_relation_object_item_id[]" size="8" />
-    </div>
-</div>
-<button type="button" class="item-relations-add-relation">Add Relation</button>
-<?php if ($subjects || $objects): ?>
-<table>
-    <tr>
-        <th>Subject</th>
-        <th>Relation</th>
-        <th>Object</th>
-        <th>Delete?</th>
-    </tr>
-    <?php foreach ($subjects as $subject): ?>
-    <?php
-    $title = item('Dublin Core', 'Title', array(), get_item_by_id($subject->object_item_id));
-    if (!$title) {
-        $title = $subject->object_item_id;
-    }
-    ?>
-    <tr>
-        <td>This Item</td>
-        <td><?php echo $subject->name; ?></td>
-        <td><a href="<?php echo uri('items/show/' . $subject->object_item_id); ?>" target="_blank"><?php echo $title; ?></a></td>
-        <td><input type="checkbox" name="item_relations_item_relation_delete[]" value="<?php echo $subject->id ?>" /></td>
-    </tr>
-    <?php endforeach; ?>
-    <?php foreach ($objects as $object): ?>
-    <?php
-    $title = item('Dublin Core', 'Title', array(), get_item_by_id($object->subject_item_id));
-    if (!$title) {
-        $title = $object->subject_item_id;
-    }
-    ?>
-    <tr>
-        <td><a href="<?php echo uri('items/show/' . $object->subject_item_id); ?>" target="_blank"><?php echo $title; ?></a></td>
-        <td><?php echo $object->name; ?></td>
-        <td>This Item</td>
-        <td><input type="checkbox" name="item_relations_item_relation_delete[]" value="<?php echo $object->id ?>" /></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
-<?php else: ?>
-<p>This item has no relations.</p>
-<?php endif; ?>
-<?php echo js('item_relations'); ?>
-<script type="text/javascript">
-    jQuery(document).ready(function () {
-        Omeka.ItemRelations.activateItemRelationButtons();
-    });
-</script>
-<?php
+        include 'item_relations_form.php';
         $content = ob_get_contents();
         ob_end_clean();
         return $content;
