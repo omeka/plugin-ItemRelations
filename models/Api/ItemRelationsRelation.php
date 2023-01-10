17f1e7064a843bc162fceecf9935b6bbbe737450
Matt Milner <milner.matthew@gmail.com>
2023-01-10 19:34:05 -0330

    Add Property ID, Item_Relations ID, and Subject / Object ItemTitles (#18)

--- models/Api/ItemRelationsRelation.php
+++ models/Api/ItemRelationsRelation.php
@@ -11,13 +11,19 @@ class Api_ItemRelationsRelation extends Omeka_Record_Api_AbstractRecordAdapter
         if ($term == ':') {
             $term = null;
         }
+        $subject=get_record_by_id('item',$record->subject_item_id);
+        $object=get_record_by_id('item',$record->object_item_id);
+        
         return array(
+            'id' => $record->id,
             'subject' => array(
                 'id' => $record->subject_item_id,
                 'url' => self::getResourceUrl("/items/{$record->subject_item_id}"),
+                'title' => ItemRelationsPlugin::getItemTitle($subject),
                 'resource' => 'items',
             ),
             'relation' => array(
+                'id' => $record->property_id,
                 'term' => $term,
                 'label' => $record->property_label,
                 'vocabulary' => $record->vocabulary_name,
@@ -25,6 +31,7 @@ class Api_ItemRelationsRelation extends Omeka_Record_Api_AbstractRecordAdapter
             'object' => array(
                 'id' => $record->object_item_id,
                 'url' => self::getResourceUrl("/items/{$record->object_item_id}"),
+                'title' => ItemRelationsPlugin::getItemTitle($object),
                 'resource' => 'items',
             ),
         );
